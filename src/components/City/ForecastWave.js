522d8540ac24332d2e6d62ef887e2628766e8758
Lucas Salomao <lucaas.afonso@hotmail.com>
2022-02-12 15:42:46 -0300

    Adicionando Get per Hour API

---
+++ src/components/City/ForecastWave.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react"
+import getAPI from "../GetAPI";
+
+export const ForecastWave = ({ id }) => {
+
+    const [waveForecastData, setWaveForecastData] = useState("")
+
+    useEffect(() => {
+        const getForecastWave = async () => {
+            const data = await getAPI.getWaveData(id)
+            setWaveForecastData(data)
+        }
+
+        if ((id !== "")) {
+            getForecastWave()
+        }
+
+    }, [id])
+
+    return (
+        <>
+            <h1>Esse é o retorno do FORECAST WAVE </h1>
+            
+        </>
+    )
+
+}
