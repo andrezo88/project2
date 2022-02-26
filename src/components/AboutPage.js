c6f3b81fb2b80fe804baca9d5a17e3b466ba5c2d
Andre Abre <andre@mundowap.com.br>
2022-02-25 23:14:49 -0300

    a

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,30 +1,14 @@
-/* import React from 'react'
-import Carousel from 'flat-carousel'
+import React from 'react'
+import { NavBar } from "./NavBar"
+import about from "./images/about.gif";
 
-export const About = () => {
-
-    const people = [
-        {
-            index: "1",
-            name: "Andre",
-            texto: "Texto aqui"
-        },
-        {
-            index: "2",
-            name: "Lucas",
-            texto: "Texto aqui"
-        }
-    ]
-
+export const AboutPage = () => {
     return (
-        <Carousel>
-        {   people.map((person) => {
-                return (
-                    <div key={person.index}> {person.name} </div>
-                )
-            })
-        }
-        </Carousel>
+        <>
+            <NavBar />
+            <div>
+                <img src={about} alt="About Gif" />
+            </div>
+        </>
     )
 }
- */
