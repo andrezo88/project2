f6f0e20842d19fb5c2b7ba6754d66eda2cec404a
Andre Abre <andre@mundowap.com.br>
2022-02-26 00:00:14 -0300

    aboutpage adicionada

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { NavBar } from "./NavBar"
-import about from "./images/about.gif";
+import about from "../components/images/about.gif"
+import "./AboutPage.css"
 
 export const AboutPage = () => {
     return (
         <>
             <NavBar />
-            <div>
-                <img src={about} alt="About Gif" />
+            <div className="about-section">
+                <div className="img-about">
+                    <img src={about} alt="About Gif" />
+                </div>
+                <div className="text-about">
+                    <div>
+                        <div className="text-about-title">About Us</div>
+                        <div className="text-about-msg">
+                            The webpage was developed by André Abreu and Lucas Salomão to innovating the way of check the weather and forecast.
+                            Making it simple and intuitive to find cities weather around the world also created
+                            to compare cities in a fun and functional way.
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
